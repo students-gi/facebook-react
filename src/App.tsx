d06707d6693f606db3baaca249985536608928fd
Gatis Ieviņš <78796801+students-gi@users.noreply.github.com>
2024-02-20 20:10:41 +0200

    Creating some basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Component imports
+import Heading1 from "./ui/atoms/Heading1";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<Heading1 />);
 }
 
 export default App;
---
+++ src/ui/atoms/Heading1.tsx
@@ -0,0 +1,9 @@
+function Heading1() {
+  let title = "Some modified heading text";
+
+  return (
+    <h1>{title}</h1>
+  );
+}
+
+export default Heading1;
