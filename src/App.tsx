85322285e0d4e13e4d86f8cdfd9aa1042c433eaf
Gatis Ieviņš <78796801+students-gi@users.noreply.github.com>
2024-02-21 18:34:42 +0200

    creating components w/ props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import './App.css';
 
 // Component imports
 import Heading1 from "./ui/atoms/Heading1";
+import Greeting from "./ui/atoms/Greeting";
 
 function App() {
-  return (<Heading1 />);
+  return (
+    <div id="App">
+      <Heading1 />
+
+      <Greeting name="Bob" />
+      <Greeting name="Jack" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ui/atoms/Greeting.tsx
@@ -0,0 +1,7 @@
+function Greeting(props:any) {
+  return (
+    <h2>Hello there, {props.name}</h2>
+  );
+}
+
+export default Greeting;
