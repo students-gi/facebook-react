97b8c21f74d3af55c2185921ca48955a9cd26c78
Gatis Ieviņš <78796801+students-gi@users.noreply.github.com>
2024-03-06 16:17:07 +0200

    creating React routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,52 @@ import Heading1 from "./ui/atoms/Heading1";
 import CardDisplay from "./ui/organisms/CardDisplay";
 import GuessANumber from "./ui/organisms/GuessANumber";
 import RegistrationForm from "./ui/organisms/RegistrationForm"
-import MealDisplay from  "./ui/organisms/MealDisplay"
-import WalletDisplay from  "./ui/organisms/WalletDisplay"
+import MealDisplay from "./ui/organisms/MealDisplay"
+import WalletDisplay from "./ui/organisms/WalletDisplay"
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div id="App">
-      <Heading1 title='Creating Cards' />
-      <CardDisplay />
-      <hr/>
-
-      <Heading1 title='Buttons and their Click events' />
-      <GuessANumber />
-      <hr/>
-
-      <Heading1 title="React hooks & forms (kinda)" />
-      <RegistrationForm />
-      <hr/>
-
-      <Heading1 title="React contexts" />
-      <MealDisplay />
-      <hr/>
-
-      <Heading1 title="React reducer hook" />
-      <WalletDisplay />
+      <nav className='top-nav'>
+        <Link to='/card'>Cards</Link>
+        <Link to='/guess'>Number Guesser</Link>
+        <Link to='/register'>Registration</Link>
+        <Link to='/meals'>Meal list</Link>
+        <Link to='/wallet'>Wallet</Link>
+      </nav>
+      <Routes>
+        <Route path='card' element={
+          <>
+            <Heading1 title='Creating Cards' />
+            <CardDisplay />
+          </>
+        } />
+        <Route path='guess' element={
+          <>
+            <Heading1 title='Buttons and their Click events' />
+            <GuessANumber />
+          </>
+        } />
+        <Route path='register' element={
+          <>
+            <Heading1 title="React hooks & forms (kinda)" />
+            <RegistrationForm />
+          </>
+        } />
+        <Route path='meals' element={
+          <>
+            <Heading1 title="React contexts" />
+            <MealDisplay />
+          </>
+        } />
+        <Route path='wallet' element={
+          <>
+            <Heading1 title="React reducer hook" />
+            <WalletDisplay />
+          </>
+        } />
+      </Routes>
     </div>
   );
 }
